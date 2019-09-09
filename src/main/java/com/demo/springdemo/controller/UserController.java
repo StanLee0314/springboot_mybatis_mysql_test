433383e097a29f2221d6121b890e20d97ed05788
Yuxuan Li <liyuxuan0314@gmail.com>
2019-09-09 16:54:45 +0800

    url加入参数

--- src/main/java/com/demo/springdemo/controller/UserController.java
+++ src/main/java/com/demo/springdemo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.demo.springdemo.model.User;
 import com.demo.springdemo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,22 +24,28 @@ public class UserController {
         return userService.listUsers();
     }
 
-    @RequestMapping(value = "/user")
-    public User selectUserById(String id) {
+    @RequestMapping(value = "/users/{id}")
+    public User selectUserById(@PathVariable String id) {
         return  userService.getUserById(Integer.parseInt(id));
 
     }
 
-    @RequestMapping(value = "/insertuser")
-    public void addUser(User user) {
+    @RequestMapping(value = "/insert/{id}/{name}/{age}")
+    public void addUser(User user,
+                        @PathVariable String id,
+                        @PathVariable String name,
+                        @PathVariable String age) {
         userService.insertUser(user);
     }
-    @RequestMapping(value = "/deleteuserbyid")
-    public void deleteUser(String id) {
+    @RequestMapping(value = "/delete/{id}")
+    public void deleteUser( @PathVariable String id) {
         userService.deleteUser(Integer.parseInt(id));
     }
-    @RequestMapping(value = "/updateuser")
-    public void updateUser(User user) {
+    @RequestMapping(value = "/update/{id}/{name}/{age}")
+    public void updateUser(User user,
+                           @PathVariable String id,
+                           @PathVariable String name,
+                           @PathVariable String age) {
         userService.updateUser(user);
     }
 }
